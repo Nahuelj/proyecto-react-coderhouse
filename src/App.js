12bc2952fce359fbbc880c6931fa349e42396a86
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-07-06 12:20:14 -0300

    rutas dinamicas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CartWidget from './components/CartWidget/CartWidget';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 
 function App() {
     return (
@@ -12,8 +13,11 @@ function App() {
                 <Navbar />
                 <CartWidget />
             </div>
+
             <Routes>
                 <Route path='/' element={<ItemListContainer greeting="Mensaje enviado desde una prop" />}/>
+                <Route path='/category/:id' element={<ItemListContainer greeting="Mensaje enviado desde una prop" />}/>
+                <Route path='/item/:id' element={<ItemDetailContainer/>}/>
             </Routes>
         </div>
         </BrowserRouter>
