b2f69bbef9a9538282a38b91516c512af9ca764f
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-07-06 11:57:11 -0300

    importado rutas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import CartWidget from './components/CartWidget/CartWidget';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
