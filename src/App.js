6143c366a723206a98dfd7f889be6a3b0344c4b8
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-07-06 15:51:03 -0300

    Segunda Pre-entrega

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import CartWidget from './components/CartWidget/CartWidget';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
 
 function App() {
+
     return (
         <BrowserRouter>
         <div className="App">
@@ -15,10 +16,11 @@ function App() {
             </div>
 
             <Routes>
-                <Route path='/' element={<ItemListContainer greeting="Mensaje enviado desde una prop" />}/>
-                <Route path='/category/:id' element={<ItemListContainer greeting="Mensaje enviado desde una prop" />}/>
+                <Route path='/' element={<ItemListContainer greeting="Shop" />}/>
+                <Route path='/category/:id' element={<ItemListContainer greeting={"Shop"} />}/>
                 <Route path='/item/:id' element={<ItemDetailContainer/>}/>
             </Routes>
+
         </div>
         </BrowserRouter>
     );
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,20 +2,27 @@ import React,{ useEffect, useState } from 'react'
 import ItemList from '../ItemList/ItemList'
 import styles from "./ItemListContainer.module.css"
 import data from "../../data/products.json";
+import { useParams } from 'react-router-dom';
+
 const ItemListContainer = ({ greeting }) => {
   const [products, setProducts] = useState([]);
+  const {id} = useParams();
 
   useEffect(() => {
     const promesa = new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve(data);
-      }, 1000);
+      }, 500);
     });
 
     promesa.then((result) => {
       setProducts(result);
+      if (id) {
+        setProducts(result.filter(item => item.categoria == id));
+
+      }
     });
-  }, []);
+  }, [id]);
 
   return (
     <div className={styles.containerItemList}>
